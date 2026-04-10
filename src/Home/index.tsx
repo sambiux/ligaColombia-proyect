bf43668e770640d8ed5873459d414e05a959b534
sambiux <sddaza.9@gmail.com>
2026-04-10 19:20:48 +0000

    Actualizano archivos

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
 
 interface Ranking {
   rank: number
@@ -7,9 +8,16 @@ interface Ranking {
   matchesPlayed: number
 }
 
+type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
+
+
 function Home() {
   const [ranking, setRanking] = useState<Ranking[]>([])
   const [title, setTitle] = useState('')
+
+
+  /*filtro*/
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +35,44 @@ function Home() {
     fetchData()
   }, [])
 
+
+  const equiposMap: Record<string, string> = {
+  "América de Cali SA": "america-de-cali",
+  "CA Bucaramanga": "atletico-bucaramanga",
+  "Club Atlético Nacional SA": "atletico-nacional",
+  "Club Deportes Tolima SA": "deportes-tolima",
+  "Asociación Deportivo Cali": "deportivo-cali",
+  "Deportivo Independiente Medellín": "independiente-medellin",
+  "Club Independiente Santa Fe": "independiente-santa-fe",
+  "CD Popular Junior FC SA": "junior",
+  "Millonarios FC": "millonarios",
+  "Once Caldas SA": "once-caldas",
+
+  "Internacional de Bogotá": "internacional-bogota",
+  "Club Llaneros SA": "llaneros",
+  "Águilas Doradas": "aguilas-doradas",
+  "Fortaleza FC": "fortaleza",
+  "Alianza FC": "alianza",
+  "Jaguares de Córdoba FC": "jaguares",
+  "Cúcuta Deportivo FC": "cucuta",
+  "Boyacá Chicó FC": "boyaca-chico",
+  "Deportivo Pereira FC": "pereira"
+}
+
   return (
+
+    <>
+     <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))}
+      </div>
     <div className="tabla-container">
       <h2>{title}</h2>
       <table className="tabla-posiciones">
@@ -43,7 +88,9 @@ function Home() {
           {ranking.map((equipo) => (
             <tr key={equipo.rank}>
               <td>{equipo.rank}</td>
-              <td>{equipo.contestantName}</td>
+              <Link to={`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+              {equipo.contestantName}
+              </Link>
               <td>{equipo.matchesPlayed}</td>
               <td>{equipo.points}</td>
             </tr>
@@ -51,6 +98,7 @@ function Home() {
         </tbody>
       </table>
     </div>
+    </>
   )
 }
 
--- src/equipo/index.tsx
+++ src/equipo/index.tsx
@@ -1,11 +1,73 @@
+import { useParams } from 'react-router';
+import { useEffect, useState} from 'react';
 import './style.css'
 
+
+
+
 function Equipo(){
+
+
+    const {equipo} = useParams<{equipo:string}>();
+
+    interface TeamData {
+    team: {
+    name: string;
+    info: {
+      city: string;
+      founded: string;
+      stadium: string;
+      president: string;
+      last_title: string;
+    };
+    ranking: {
+      position: string;
+      competition: string;
+    };
+    social: {
+      facebook: string;
+      instagram: string;
+      x: string;
+    };
+    links: {
+      store: string;
+      tickets: string;
+    };
+  };
+}
+
+    const [data, setData] = useState<TeamData | null>(null)
+
+    useEffect(() => {
+    if (!equipo) return;
+
+    const fetchData = async () => {
+    try {
+    const res = await fetch(
+        `https://raw.githubusercontent.com/sdtibata/dataliga/main/${equipo}.json`
+      );
+
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.error("Error cargando datos:", error);
+    }
+  };
+
+  fetchData();
+}, [equipo]);
+
+       if (!data) return <p>Cargando...</p>;
+
     return(
         <>
-            <p>Equipo</p>
+            <p>{equipo}</p>
+            <p>{data.team.name}</p>
+            <p>{data.team.info.city}</p>
         </>
     )
 }
+
+
 
 export default Equipo;
