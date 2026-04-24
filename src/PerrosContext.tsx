ef2bb8aa62dd114816350f4c336a252eb6e44a7d
sambiux <sddaza.9@gmail.com>
2026-04-24 01:36:55 +0000

    Actualizando importaciones

--- src/PerrosContext.tsx
+++ src/PerrosContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from "react";
+import { createContext, useEffect, useState } from "react";
+import type { ReactNode } from "react";
 
 export interface userData {
   id: string;
